eab52b1bc948fdf68656ec84751e177b4ece18f6
evanbones <74276616+evanbones@users.noreply.github.com>
2025-12-22 05:31:54 -0800

    Voline animation and behaviour improvements

--- common/src/main/java/com/infernalstudios/infernalexp/client/entity/model/VolineModel.java
+++ common/src/main/java/com/infernalstudios/infernalexp/client/entity/model/VolineModel.java
@@ -4,34 +4,35 @@ import com.infernalstudios.infernalexp.IEConstants;
 import com.infernalstudios.infernalexp.entities.VolineEntity;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
-import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.model.HierarchicalModel;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.*;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.Mth;
 import org.jetbrains.annotations.NotNull;
 
-public class VolineModel<T extends VolineEntity> extends EntityModel<T> {
+public class VolineModel<T extends VolineEntity> extends HierarchicalModel<T> {
 
     public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(
             new ResourceLocation(IEConstants.MOD_ID, "voline"), "main");
 
+    private final ModelPart root;
     private final ModelPart body;
-    private final ModelPart mouth;
-    private final ModelPart leg1;
-    private final ModelPart leg2;
-    private final ModelPart leg3;
-    private final ModelPart leg4;
+    private final ModelPart head; // Renamed from 'mouth' to match animation
+    private final ModelPart frontRightLeg;
+    private final ModelPart frontLeftLeg;
+    private final ModelPart backRightLeg;
+    private final ModelPart backLeftLeg;
 
     public VolineModel(ModelPart root) {
+        this.root = root;
         this.body = root.getChild("body");
-        this.mouth = root.getChild("mouth");
-        this.leg1 = root.getChild("leg_1");
-        this.leg2 = root.getChild("leg_2");
-        this.leg3 = root.getChild("leg_3");
-        this.leg4 = root.getChild("leg_4");
+        this.head = root.getChild("head");
+        this.frontRightLeg = root.getChild("frontrightleg");
+        this.frontLeftLeg = root.getChild("frontleftleg");
+        this.backRightLeg = root.getChild("backrightleg");
+        this.backLeftLeg = root.getChild("backleftleg");
     }
 
     public static LayerDefinition createBodyLayer() {
@@ -39,9 +40,9 @@ public class VolineModel<T extends VolineEntity> extends EntityModel<T> {
         PartDefinition partDefinition = meshDefinition.getRoot();
 
         PartDefinition body = partDefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -8.0F, -8.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 20.0F, 4.0F));
-        PartDefinition mouth = partDefinition.addOrReplaceChild("mouth", CubeListBuilder.create().texOffs(0, 16).addBox(-4.0F, -5.0F, -4.0F, 8.0F, 3.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
+        PartDefinition head = partDefinition.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 16).addBox(-4.0F, -5.0F, -4.0F, 8.0F, 3.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
 
-        mouth.addOrReplaceChild("mouth_inside", CubeListBuilder.create().texOffs(0, 27).addBox(-4.0F, -4.0F, 1.0F, 4.0F, 4.0F, 0.0F, new CubeDeformation(0.0F))
+        head.addOrReplaceChild("mouth_inside", CubeListBuilder.create().texOffs(0, 27).addBox(-4.0F, -4.0F, 1.0F, 4.0F, 4.0F, 0.0F, new CubeDeformation(0.0F))
                 .texOffs(8, 27).addBox(0.0F, -4.0F, 1.0F, 4.0F, 4.0F, 0.0F, new CubeDeformation(0.0F))
                 .texOffs(16, 27).addBox(0.0F, 0.0F, 1.0F, 4.0F, 4.0F, 0.0F, new CubeDeformation(0.0F))
                 .texOffs(24, 27).addBox(-4.0F, 0.0F, 1.0F, 4.0F, 4.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -4.0F, 0.0F, -1.5708F, 0.0F, 0.0F));
@@ -51,34 +52,31 @@ public class VolineModel<T extends VolineEntity> extends EntityModel<T> {
                 .texOffs(16, 27).addBox(0.0F, 0.0F, 1.0F, 4.0F, 4.0F, 0.0F, new CubeDeformation(0.0F))
                 .texOffs(24, 27).addBox(-4.0F, 0.0F, 1.0F, 4.0F, 4.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, -2.0F, -4.0F, -1.5708F, 0.0F, 0.0F));
 
-        partDefinition.addOrReplaceChild("leg_1", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0F, 0.0F, -1.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(-2.0F, 22.0F, -2.0F));
-        partDefinition.addOrReplaceChild("leg_2", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0F, 0.0F, -1.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(2.0F, 22.0F, -2.0F));
-        partDefinition.addOrReplaceChild("leg_3", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0F, 0.0F, -1.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(-2.0F, 22.0F, 2.0F));
-        partDefinition.addOrReplaceChild("leg_4", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0F, 0.0F, -1.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(2.0F, 22.0F, 2.0F));
+        partDefinition.addOrReplaceChild("frontrightleg", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0F, 0.0F, -1.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(-2.0F, 22.0F, -2.0F));
+        partDefinition.addOrReplaceChild("frontleftleg", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0F, 0.0F, -1.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(2.0F, 22.0F, -2.0F));
+        partDefinition.addOrReplaceChild("backrightleg", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0F, 0.0F, -1.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(-2.0F, 22.0F, 2.0F));
+        partDefinition.addOrReplaceChild("backleftleg", CubeListBuilder.create().texOffs(0, 0).addBox(-1.0F, 0.0F, -1.0F, 2.0F, 2.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(2.0F, 22.0F, 2.0F));
 
         return LayerDefinition.create(meshDefinition, 32, 32);
     }
 
     @Override
-    public void setupAnim(T entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
-        leg1.xRot = Mth.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
-        leg2.xRot = Mth.cos(limbSwing * 0.6662F + (float) Math.PI) * 1.4F * limbSwingAmount;
-        leg3.xRot = Mth.cos(limbSwing * 0.6662F + (float) Math.PI) * 1.4F * limbSwingAmount;
-        leg4.xRot = Mth.cos(limbSwing * 0.6662F) * 1.4F * limbSwingAmount;
-        body.xRot = -Mth.abs(Mth.cos(limbSwing * 0.4662F) * 1.4F * limbSwingAmount);
+    public ModelPart root() {
+        return this.root;
+    }
 
-        if (entityIn.isEating()) {
-            body.xRot = (Mth.cos(ageInTicks) * (float) Math.PI * 0.1F);
-        }
+    @Override
+    public void setupAnim(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+        this.root().getAllParts().forEach(ModelPart::resetPose);
+
+        this.animate(entity.idleAnimationState, VolineAnimation.idle, ageInTicks);
+        this.animate(entity.walkAnimationState, VolineAnimation.walk, ageInTicks);
+        this.animate(entity.eatAnimationState, VolineAnimation.eating, ageInTicks);
+        this.animate(entity.sleepAnimationState, VolineAnimation.sleeping, ageInTicks);
     }
 
     @Override
     public void renderToBuffer(@NotNull PoseStack matrixStack, @NotNull VertexConsumer buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
-        body.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
-        mouth.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
-        leg1.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
-        leg2.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
-        leg3.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
-        leg4.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
+        this.root.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
     }
 }
--- common/src/main/java/com/infernalstudios/infernalexp/entities/VolineEntity.java
+++ common/src/main/java/com/infernalstudios/infernalexp/entities/VolineEntity.java
@@ -11,13 +11,16 @@ import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.*;
+import net.minecraft.world.entity.AnimationState;
+import net.minecraft.world.entity.EntityDimensions;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Pose;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.*;
@@ -31,25 +34,24 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.function.Predicate;
+import java.util.Objects;
 
 public class VolineEntity extends Monster implements IBucketable {
     public static final EntityDataAccessor<Integer> MAGMA_CREAM_EATEN = SynchedEntityData.defineId(VolineEntity.class, EntityDataSerializers.INT);
     private static final EntityDataAccessor<Boolean> IS_SLEEPING = SynchedEntityData.defineId(VolineEntity.class, EntityDataSerializers.BOOLEAN);
     private static final EntityDataAccessor<Boolean> IS_EATING = SynchedEntityData.defineId(VolineEntity.class, EntityDataSerializers.BOOLEAN);
     private static final EntityDataAccessor<Boolean> FROM_BUCKET = SynchedEntityData.defineId(VolineEntity.class, EntityDataSerializers.BOOLEAN);
+    private static final EntityDataAccessor<Integer> SLEEP_TIMER = SynchedEntityData.defineId(VolineEntity.class, EntityDataSerializers.INT);
 
-    private final Predicate<LivingEntity> TARGET_CONDITION = entity -> {
-        if (this.entityData.get(MAGMA_CREAM_EATEN) > 0) return false;
-
-        return entity.hasEffect(MobEffects.FIRE_RESISTANCE) && !entity.isHolding(Items.MAGMA_CREAM);
-    };
+    // Animation States
+    public final AnimationState idleAnimationState = new AnimationState();
+    public final AnimationState walkAnimationState = new AnimationState();
+    public final AnimationState eatAnimationState = new AnimationState();
+    public final AnimationState sleepAnimationState = new AnimationState();
 
     public VolineEntity(EntityType<? extends Monster> type, Level level) {
         super(type, level);
@@ -66,29 +68,71 @@ public class VolineEntity extends Monster implements IBucketable {
         this.entityData.define(IS_SLEEPING, false);
         this.entityData.define(IS_EATING, false);
         this.entityData.define(FROM_BUCKET, false);
+        this.entityData.define(SLEEP_TIMER, 0);
     }
 
     @Override
     protected void registerGoals() {
-        this.goalSelector.addGoal(0, new PanicGoal(this, 1.5D));
-        this.goalSelector.addGoal(1, new FloatGoal(this));
+        super.registerGoals();
+        this.goalSelector.addGoal(1, new EatItemsGoal(this));
         this.goalSelector.addGoal(2, new TemptGoal(this, 1.2D, Ingredient.of(Items.MAGMA_CREAM), false));
         this.goalSelector.addGoal(3, new AvoidEntityGoal<>(this, Player.class, 6.0F, 1.2D, 1.5D, entity -> entity.isHolding(Items.SNOWBALL)));
         this.goalSelector.addGoal(3, new AvoidEntityGoal<>(this, Piglin.class, 8.0F, 1.0D, 1.2D, entity -> !entity.hasEffect(MobEffects.FIRE_RESISTANCE)));
-        this.goalSelector.addGoal(4, new EatItemsGoal(this));
-        this.goalSelector.addGoal(5, new MeleeAttackGoal(this, 1.2D, true));
-        this.goalSelector.addGoal(6, new WaterAvoidingRandomStrollGoal(this, 1.0D));
-        this.goalSelector.addGoal(7, new LookAtPlayerGoal(this, Player.class, 8.0F));
-        this.goalSelector.addGoal(8, new RandomLookAroundGoal(this));
-        this.targetSelector.addGoal(1, new HurtByTargetGoal(this));
-        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, 10, true, false, TARGET_CONDITION));
-        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, LivingEntity.class, 10, true, false, TARGET_CONDITION));
+        this.goalSelector.addGoal(4, new MeleeAttackGoal(this, 1.2D, true));
+        this.goalSelector.addGoal(5, new WaterAvoidingRandomStrollGoal(this, 1.0D));
+        this.goalSelector.addGoal(6, new LookAtPlayerGoal(this, Player.class, 8.0F));
+        this.goalSelector.addGoal(7, new RandomLookAroundGoal(this));
+        this.goalSelector.addGoal(8, new PanicGoal(this, getAttributeValue(Attributes.MOVEMENT_SPEED) * 2.0D));
+        this.targetSelector.addGoal(0, new HurtByTargetGoal(this));
+        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, Player.class, true, entity -> entity.hasEffect(MobEffects.FIRE_RESISTANCE) && !entity.isHolding(Items.MAGMA_CREAM)));
+        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Monster.class, true, entity -> entity.hasEffect(MobEffects.FIRE_RESISTANCE)));
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+        if (this.level().isClientSide()) {
+            // Handle Animation States
+            if (this.isSleeping()) {
+                this.sleepAnimationState.startIfStopped(this.tickCount);
+                this.idleAnimationState.stop();
+                this.walkAnimationState.stop();
+                this.eatAnimationState.stop();
+            } else if (this.isEating()) {
+                this.eatAnimationState.startIfStopped(this.tickCount);
+                this.idleAnimationState.stop();
+                this.walkAnimationState.stop();
+                this.sleepAnimationState.stop();
+            } else if (this.getDeltaMovement().horizontalDistanceSqr() > 1.0E-6) {
+                this.walkAnimationState.startIfStopped(this.tickCount);
+                this.idleAnimationState.stop();
+                this.eatAnimationState.stop();
+                this.sleepAnimationState.stop();
+            } else {
+                this.idleAnimationState.startIfStopped(this.tickCount);
+                this.walkAnimationState.stop();
+                this.eatAnimationState.stop();
+                this.sleepAnimationState.stop();
+            }
+        }
     }
 
     @Override
     public void onSyncedDataUpdated(@NotNull EntityDataAccessor<?> key) {
         if (MAGMA_CREAM_EATEN.equals(key)) {
             this.refreshDimensions();
+        } else if (IS_SLEEPING.equals(key)) {
+            if (this.isSleeping()) {
+                float snappedYaw = Math.round(this.getYRot() / 90.0F) * 90.0F;
+                this.setYRot(snappedYaw);
+                this.yBodyRot = snappedYaw;
+                this.yHeadRot = snappedYaw;
+                this.yRotO = snappedYaw;
+                this.yBodyRotO = snappedYaw;
+                this.yHeadRotO = snappedYaw;
+            } else {
+                this.sleepAnimationState.stop();
+            }
         }
         super.onSyncedDataUpdated(key);
     }
@@ -99,13 +143,16 @@ public class VolineEntity extends Monster implements IBucketable {
     }
 
     public float getSizeFactor() {
+        if (this.isSleeping()) {
+            float progress = (float) this.entityData.get(SLEEP_TIMER) / 12000.0F;
+            return 1.0F + (0.6F * progress);
+        }
         return 1.0F + (this.entityData.get(MAGMA_CREAM_EATEN) * 0.2F);
     }
 
     @Override
     public void refreshDimensions() {
         super.refreshDimensions();
-        // Prevent suffocation when growing
         this.setPos(this.getX(), this.getY(), this.getZ());
     }
 
@@ -125,11 +172,25 @@ public class VolineEntity extends Monster implements IBucketable {
     }
 
     public boolean wantsToEat(ItemStack stack) {
-        return stack.is(Items.MAGMA_CREAM) || stack.is(Items.GOLD_NUGGET) || stack.is(Items.GOLD_INGOT);
+        return !this.isSleeping() && (stack.is(Items.MAGMA_CREAM) || stack.is(Items.GOLD_NUGGET) || stack.is(Items.GOLD_INGOT));
     }
 
     public boolean isSleeping() {
         return this.entityData.get(IS_SLEEPING);
+    }
+
+    private void wakeUp() {
+        this.entityData.set(IS_SLEEPING, false);
+        this.entityData.set(MAGMA_CREAM_EATEN, 0);
+        Objects.requireNonNull(this.getAttribute(Attributes.MOVEMENT_SPEED)).setBaseValue(0.5D);
+        this.removeEffect(MobEffects.MOVEMENT_SLOWDOWN);
+    }
+
+    @Override
+    protected void jumpFromGround() {
+        if (!this.isSleeping()) {
+            super.jumpFromGround();
+        }
     }
 
     public void ate(ItemStack stack) {
@@ -137,14 +198,19 @@ public class VolineEntity extends Monster implements IBucketable {
             int eaten = this.entityData.get(MAGMA_CREAM_EATEN) + 1;
             this.entityData.set(MAGMA_CREAM_EATEN, eaten);
 
-            this.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 6000, eaten - 1));
+            this.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 6000, eaten - 1, false, false));
             this.setPersistenceRequired();
             this.setTarget(null);
 
             if (eaten >= 3) {
                 this.entityData.set(IS_SLEEPING, true);
+                this.entityData.set(SLEEP_TIMER, 12000);
                 this.getNavigation().stop();
+                Objects.requireNonNull(this.getAttribute(Attributes.MOVEMENT_SPEED)).setBaseValue(0.0D);
                 this.setDeltaMovement(0, 0, 0);
+
+                BlockPos pos = this.blockPosition();
+                this.setPos(pos.getX() + 0.5D, this.getY(), pos.getZ() + 0.5D);
             }
         }
     }
@@ -192,6 +258,9 @@ public class VolineEntity extends Monster implements IBucketable {
 
     @Override
     protected SoundEvent getAmbientSound() {
+        if (this.isSleeping()) {
+            return null;
+        }
         return ModSounds.VOLINE_AMBIENT.get();
     }
 
@@ -212,12 +281,18 @@ public class VolineEntity extends Monster implements IBucketable {
 
     @Override
     public boolean hurt(DamageSource source, float amount) {
-        if (source.getDirectEntity() instanceof Snowball) {
-            if (this.entityData.get(IS_SLEEPING)) {
+        boolean isSnowball = source.getDirectEntity() instanceof Snowball;
+        if (isSnowball) {
+            amount += 3;
+        }
+        if (this.isSleeping()) {
+            if (source.getDirectEntity() instanceof Snowball) {
                 this.transformToGeyser();
                 return false;
             }
-            amount += 3.0F;
+            if (source != this.damageSources().fellOutOfWorld()) {
+                return false;
+            }
         }
         return super.hurt(source, amount);
     }
@@ -238,6 +313,7 @@ public class VolineEntity extends Monster implements IBucketable {
         tag.putInt("MagmaCreamEaten", this.entityData.get(MAGMA_CREAM_EATEN));
         tag.putBoolean("IsSleeping", this.entityData.get(IS_SLEEPING));
         tag.putBoolean("FromBucket", this.isFromBucket());
+        tag.putInt("SleepTimer", this.entityData.get(SLEEP_TIMER));
     }
 
     @Override
@@ -246,6 +322,9 @@ public class VolineEntity extends Monster implements IBucketable {
         this.entityData.set(MAGMA_CREAM_EATEN, tag.getInt("MagmaCreamEaten"));
         this.entityData.set(IS_SLEEPING, tag.getBoolean("IsSleeping"));
         this.setFromBucket(tag.getBoolean("FromBucket"));
+        if (tag.contains("SleepTimer")) {
+            this.entityData.set(SLEEP_TIMER, tag.getInt("SleepTimer"));
+        }
     }
 
     @Nullable
@@ -256,11 +335,31 @@ public class VolineEntity extends Monster implements IBucketable {
     }
 
     @Override
+    public boolean canAttack(@NotNull LivingEntity target) {
+        return !this.isSleeping() && super.canAttack(target);
+    }
+
+    @Override
     public void aiStep() {
-        if (this.entityData.get(IS_SLEEPING)) {
-            this.setDeltaMovement(0, this.getDeltaMovement().y, 0);
-            return;
-        }
         super.aiStep();
+        if (this.isSleeping()) {
+            // Lock rotation and movement
+            this.setDeltaMovement(0, this.getDeltaMovement().y, 0);
+            this.yBodyRot = this.yBodyRotO;
+            this.yHeadRot = this.yHeadRotO;
+            this.setYRot(this.yRotO);
+            this.setXRot(this.xRotO);
+
+            // Handle timer
+            int timer = this.entityData.get(SLEEP_TIMER);
+            if (timer > 0) {
+                this.entityData.set(SLEEP_TIMER, timer - 1);
+                if (timer % 20 == 0) {
+                    this.refreshDimensions();
+                }
+            } else {
+                this.wakeUp();
+            }
+        }
     }
 }
