543e62697c2dc29e98ad7378b73c9760d08fba05
evanbones <74276616+evanbones@users.noreply.github.com>
2026-01-01 03:01:21 -0800

    Fix Terrablender causing bedrock layers to be missing

--- common/src/main/java/com/infernalstudios/infernalexp/compat/TerraBlenderCompat.java
+++ common/src/main/java/com/infernalstudios/infernalexp/compat/TerraBlenderCompat.java
@@ -9,6 +9,5 @@ import terrablender.api.SurfaceRuleManager;
 public class TerraBlenderCompat {
     public static void register() {
         Regions.register(new TerraBlenderRegion(new ResourceLocation(IEConstants.MOD_ID, "nether"), 4));
-        SurfaceRuleManager.addSurfaceRules(SurfaceRuleManager.RuleCategory.NETHER, IEConstants.MOD_ID, ModSurfaceRules.addNetherSurfaceRules());
-    }
+        SurfaceRuleManager.addSurfaceRules(SurfaceRuleManager.RuleCategory.NETHER, IEConstants.MOD_ID, ModSurfaceRules.addNetherSurfaceRulesWithBedrock());    }
 }
