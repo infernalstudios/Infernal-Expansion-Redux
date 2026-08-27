8e03fb4fac5363850a9bad86f91fb3d8ce86e60f
evanbones <74276616+evanbones@users.noreply.github.com>
2026-08-27 14:14:17 -0700

    0.3.8

--- common/src/main/java/com/infernalstudios/infernalexp/resources/config/ConfiguredData.java
+++ common/src/main/java/com/infernalstudios/infernalexp/resources/config/ConfiguredData.java
@@ -137,11 +137,11 @@ public class ConfiguredData {
                 json -> Common.createPacking3x3("netherexp:shroomnight", "infernalexp:shroomnight_tear"));
 
         register(ResourceLocation.tryBuild("infernalexp", "recipes/shroomblight_from_tears.json"),
-                () -> Services.PLATFORM.isModLoaded("netherexp"),
+                () -> (Services.PLATFORM.isModLoaded("netherexp") && Services.PLATFORM.isModLoaded("gardens_of_the_dead")),
                 json -> Common.createPacking3x3("netherexp:shroomblight", "infernalexp:shroomblight_tear"));
 
         register(ResourceLocation.tryBuild("infernalexp", "recipes/shroombright_from_tears.json"),
-                () -> Services.PLATFORM.isModLoaded("netherexp"),
+                () -> (Services.PLATFORM.isModLoaded("netherexp") && Services.PLATFORM.isModLoaded("cinderscapes")),
                 json -> Common.createPacking3x3("netherexp:shroombright", "infernalexp:shroombright_tear"));
 
         register(ResourceLocation.tryBuild("infernalexp", "loot_tables/blocks/shroomlight_tear.json"),
